b63af03d71ab5d1c7d74c2a3bccd9f168ea02a9a
Ender Loc Phan <weintome@gmail.com>
2017-01-08 18:46:34 +0200

    Add files via upload

---
+++ Assignment-2/Cell.cpp
@@ -0,0 +1,44 @@
+#include "Grid.hpp"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+Cell::Cell()
+{
+}
+Cell::Cell(bool alive_cell,int x, int y) {
+
+	this->shape = RectangleShape(Vector2f(15, 15));
+	this->shape.setPosition(x, y);
+	this->shape.setFillColor(Color::Blue);
+	this->shape.setOutlineThickness(1);
+	this->shape.setOutlineColor(Color::Black);
+
+	this->alive_cell = alive_cell;
+
+	if (alive_cell)
+		this->shape.setFillColor(Color::White);
+	else
+		this->shape.setFillColor(Color::Blue);
+
+}
+void Cell::modify_cell(bool alive_cell) {
+
+	this->alive_cell = alive_cell;
+
+	if (alive_cell)
+		this->shape.setFillColor(Color::White);
+	else
+		this->shape.setFillColor(Color::Blue);
+}
+
+RectangleShape* Cell::getShape() { return &shape; };
+
+void Cell::draw(RenderWindow &window) {
+
+	window.draw(shape);
+}
+bool Cell :: Alive() {
+
+	return alive_cell;
+}
+
